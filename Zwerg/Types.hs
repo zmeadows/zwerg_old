4dfbb7fdc3bb3940c0971c8e2dc06f266fd8def8
Zachary Meadows <zac@Zacharys-MacBook-Pro.local>
2015-04-28 01:22:07 -0400

    expanding Glyph data type

--- Zwerg/Types.hs
+++ Zwerg/Types.hs
@@ -1,12 +1,15 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Zwerg.Types where
 
+import Data.Label
 import Data.Text(Text)
 
 data Direction = North | South | East | West deriving (Show, Read, Eq)
 
-data Color = Default | White | Black | Green | Red | Blue deriving (Show, Read, Eq)
+data Color =  White | Black | Red | Green | Blue deriving (Show, Read, Eq, Ord)
 
-data KeyCode = Letter Char | SemiColon | Escape deriving (Eq, Show)
+data KeyCode = Letter Char | Return | Escape deriving (Eq, Show)
 
 data KeyMod = Shift | Control | None deriving (Show, Eq)
 
@@ -14,7 +17,18 @@ data TileType = Wall | Door | Floor | Void deriving (Show, Read, Eq)
 
 type Position = (Int,Int)
 
-type Glyph = (Char, Color, Color)
+data FontType = FontRegular | FontBold | FontItalic | FontBoldItalic deriving (Show, Read, Eq, Ord)
+
+fclabels [d|
+    data Glyph = Glyph {
+        character :: Char,
+        fontType  :: FontType,
+        foreground :: Color,
+        background :: Color,
+        foregroundAlpha :: Double,
+        backgroundAlpha :: Double
+        } deriving (Show, Read, Eq, Ord)
+  |]
 
 data ZWERGError = PlayerError Text
                 | FatalError Integer Text deriving (Show, Eq)
