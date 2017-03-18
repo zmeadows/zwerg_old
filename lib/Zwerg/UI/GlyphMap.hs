9514b83d912860f949e5a6160241b5291f5e52d6
Zachary Meadows <zmeadows@gmail.com>
2017-03-18 18:45:51 -0400

    basic utility functions for creating glyph maps

--- lib/Zwerg/UI/GlyphMap.hs
+++ lib/Zwerg/UI/GlyphMap.hs
@@ -4,6 +4,7 @@ import Zwerg.Component.Position (Position)
 import Zwerg.Component.Glyph (Glyph)
 
 import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M (empty, fromList)
 import GHC.Generics (Generic)
 import Data.Binary
 
@@ -11,3 +12,9 @@ newtype GlyphMap = MkGlyphMap (Map Position Glyph)
     deriving (Show, Read, Eq, Generic)
 
 instance Binary GlyphMap
+
+emptyGlyphMap :: GlyphMap
+emptyGlyphMap = MkGlyphMap M.empty
+
+mkGlyphMap :: [(Position, Glyph)] -> GlyphMap
+mkGlyphMap = MkGlyphMap . M.fromList
