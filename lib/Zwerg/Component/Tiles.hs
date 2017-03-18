9828de502b4d8ffd39a3487d3f6780e4f0757d59
Zachary Meadows <zmeadows@gmail.com>
2017-03-18 18:42:09 -0400

    revamping for tile vector filtering and Int -> UUID

--- lib/Zwerg/Component/Tiles.hs
+++ lib/Zwerg/Component/Tiles.hs
@@ -1,15 +1,16 @@
 module Zwerg.Component.Tiles (
     Tiles,
     mkTiles,
-    getTileUUIDAtPos
+    getTileUUIDAtPos,
+    filterTilesM
     ) where
 
 import Zwerg.Component.Position
 import Zwerg.Component.UUID
 
-import Data.Vector.Unboxed (Vector)
+import Data.Vector (Vector)
 import Data.Vector.Binary()
-import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector as V
 
 import Control.Exception.Base (assert)
 
@@ -18,14 +19,14 @@ import Data.Binary
 
 import qualified Data.List as L (sortBy, nub)
 
-newtype Tiles = MkTiles (Vector Int)
+newtype Tiles = MkTiles (Vector UUID)
     deriving (Show, Read, Eq, Generic)
 
 instance Binary Tiles
 
 mkTiles :: [(UUID, Position)] -> Tiles
 mkTiles upl = assert isValidTileList
-                $ MkTiles $ V.fromList $ map (unUUID . fst) upl'
+                $ MkTiles $ V.fromList $ map fst upl'
     where upl' = L.sortBy (\(_,p1) (_,p2) -> positionSorter p1 p2) upl
           isValidTileList = let ps = map snd upl
                             in length (L.nub ps) == length ps
@@ -39,4 +40,9 @@ positionSorter p1 p2
         pi2 = to1DIndex p2
 
 getTileUUIDAtPos :: Position -> Tiles -> UUID
-getTileUUIDAtPos p (MkTiles v) = mkUUID $ v V.! to1DIndex p
+getTileUUIDAtPos p (MkTiles v) = v V.! to1DIndex p
+
+filterTilesM :: Monad m => (UUID -> m Bool) -> Tiles -> m [UUID]
+filterTilesM f (MkTiles t) = V.toList <$> V.filterM f t
+
+
