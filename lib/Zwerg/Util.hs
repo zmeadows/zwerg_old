9eda83156ce9d4dd861f6c19060e88b9557f822f
Zachary Meadows <zmeadows@gmail.com>
2017-07-23 16:26:06 +0200

    move geometry functions to own module

--- lib/Zwerg/Util.hs
+++ lib/Zwerg/Util.hs
@@ -6,7 +6,22 @@ import Control.Exception.Base (assert)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Paths_zwerg (getDataFileName)
 
-import Unsafe
+import Unsafe (unsafeLast)
+import Data.Typeable (typeOf)
+import Data.List (words)
+import Language.Haskell.TH
+import Language.Haskell.TH.Quote
+import Language.Haskell.TH.Syntax
+import Language.Haskell.TH.Lib
+
+constrRecord :: Typeable a => a -> ExpQ
+constrRecord x = reify exp where
+    reify   = \(Just r) -> appE r $ conE $ mkName $ unsafeLast args
+    exp     = foldl (dot) uncur $ replicate terms uncur
+    terms   = ((length args) `div` 2) - 2
+    dot x y = (Just $ infixE x (varE $ mkName ".") y)
+    uncur   = (Just [|uncurry|])
+    args    = words . show $ typeOf x
 
 getAsset
   :: MonadIO m
@@ -47,24 +62,12 @@ chunksOf i ls = map (take i) (build (splitter ls))
     splitter [] _ n = n
     splitter l c n = l `c` splitter (drop i l) c n
 
--- | See <http://roguebasin.roguelikedevelopment.org/index.php/Digital_lines>.
-balancedWord :: Int -> Int -> Int -> [Int]
-balancedWord p q eps
-  | eps + p < q = 0 : balancedWord p q (eps + p)
-  | otherwise = 1 : balancedWord p q (eps + p - q)
-
--- | Bresenham's line algorithm.
--- Includes the first point and goes through the second to infinity.
-bla :: (Int, Int) -> (Int, Int) -> [(Int, Int)]
-bla (x0, y0) (x1, y1) =
-  let (dx, dy) = (x1 - x0, y1 - y0)
-      xyStep b (x, y) = (x + signum dx, y + signum dy * b)
-      yxStep b (x, y) = (x + signum dx * b, y + signum dy)
-      (p, q, step)
-        | abs dx > abs dy = (abs dy, abs dx, xyStep)
-        | otherwise = (abs dx, abs dy, yxStep)
-      walk w xy = xy : walk (unsafeTail w) (step (unsafeHead w) xy)
-  in walk (balancedWord p q 0) (x0, y0)
-
-line :: (Int, Int) -> (Int, Int) -> [(Int, Int)]
-line p1 p2 = p2 : takeWhile (/= p2) (bla p1 p2)
+takeWhileM1
+  :: (Monad m)
+  => (a -> m Bool) -> [a] -> m [a]
+takeWhileM1 _ [] = return []
+takeWhileM1 p (x:xs) = do
+  q <- p x
+  if q
+    then (takeWhileM1 p xs) >>= (return . (:) x)
+    else return [x]
