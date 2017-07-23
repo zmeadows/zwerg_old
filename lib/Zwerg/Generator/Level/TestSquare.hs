81d9a7a689f14002f909c9d34813dd78923b981c
Zachary Meadows <zmeadows@gmail.com>
2017-07-23 16:24:40 +0200

    cleanup

--- lib/Zwerg/Generator/Level/TestSquare.hs
+++ lib/Zwerg/Generator/Level/TestSquare.hs
@@ -6,41 +6,39 @@ import Zwerg.Generator.Enemy.Goblin
 import Zwerg.Generator.Level
 import Zwerg.Util
 
-import Data.Map.Strict (traverseWithKey)
+import Data.Map.Lazy (traverseWithKey)
 
 testSquareGenerator :: Generator UUID
-testSquareGenerator =
-  MkGenerator $ do
+testSquareGenerator = MkGenerator $ do
     testSquareLevelUUID <- popUUID
     generate $ levelSkeletonGenerator testSquareLevelUUID
     testSquareTiles <- demandComp tileMap testSquareLevelUUID
-    _ <-
-      flip traverseWithKey (unwrap testSquareTiles) $ \pos tileUUID -> do
+    void $ flip traverseWithKey (unwrap testSquareTiles) $ \pos tileUUID -> do
         let (x, y) = unwrap pos
-            isWallTile =
-              x == 0 || x == mapWidthINT - 1 || y == 0 || y == mapHeightINT - 1
+            isWallTile = x == 0 || x == mapWidthINT - 1 || y == 0 || y == mapHeightINT - 1
         if isWallTile
           then do
             setComp tileUUID tileType Wall
             setComp tileUUID blocksPassage True
-            setComp tileUUID glyph $
-              Glyph 'X' White2 White0 (Just Black2) (Just Black0)
+            setComp tileUUID glyph $ Glyph 'X' White2 White0 (Just Black2) (Just Black0)
           else do
             setComp tileUUID tileType Floor
             setComp tileUUID blocksPassage False
-            setComp tileUUID glyph $
-              Glyph '·' White2 White0 (Just Black2) (Just Black0)
-    replicateM_ 50 $ do
+            setComp tileUUID glyph $ Glyph '·' White2 White0 (Just Black2) (Just Black0)
+    replicateM_ 150 $ do
       goblinUUID <- generate goblin
+      -- TODO factor out function to add any generated enemy UUID to random open tile
       addComp goblinUUID level testSquareLevelUUID
       goblinTileUUID <- getRandomEmptyTile testSquareLevelUUID
+      -- TODO modify fromJustErrM to be a template haskell'd function
       goblinTileUUID' <-
         fromJustErrM goblinTileUUID $
         ZError
           __FILE__
           __LINE__
-          Fatal
+          EngineFatal
           "Could not find an open tile to place Goblin"
       demandComp position goblinTileUUID' >>= addComp goblinUUID position
       addOccupant goblinUUID goblinTileUUID'
+
     return testSquareLevelUUID
