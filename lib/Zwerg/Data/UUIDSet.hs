994908fadd6330f8f9e02037b85f211cd549151e
Zachary Meadows <zmeadows@gmail.com>
2017-07-23 16:21:48 +0200

    switch to internal list representation for speed/simplicity for now

--- lib/Zwerg/Data/UUIDSet.hs
+++ lib/Zwerg/Data/UUIDSet.hs
@@ -1,35 +1,36 @@
 module Zwerg.Data.UUIDSet where
 
-import Zwerg.Prelude hiding (toList, empty, filterM)
+import Zwerg.Prelude
 
 import qualified Control.Monad as CM (filterM)
-import Data.Set (Set)
-import qualified Data.Set as S
+import qualified Data.List as L (delete)
 
 newtype UUIDSet =
-  MkUUIDSet (Set UUID)
+  MkUUIDSet ([UUID])
   deriving (Eq, Ord, Read, Show, Monoid, Generic)
 
 instance Binary UUIDSet
 
-instance ZWrapped UUIDSet (Set UUID) where
+instance ZWrapped UUIDSet ([UUID]) where
   unwrap (MkUUIDSet us) = us
 
 instance ZContainer UUIDSet UUID where
-  zAdd uuid (MkUUIDSet us) = MkUUIDSet $ S.insert uuid us
-  zDelete uuid (MkUUIDSet us) = MkUUIDSet $ S.delete uuid us
-  zMember uuid (MkUUIDSet us) = S.member uuid us
+  zAdd uuid (MkUUIDSet us) =
+    if (elem uuid us)
+       then MkUUIDSet us
+       else MkUUIDSet (uuid:us)
+  zDelete uuid (MkUUIDSet us) = MkUUIDSet $ L.delete uuid us
+  zMember uuid (MkUUIDSet us) = elem uuid us
 
 instance ZFilterable UUIDSet UUID where
-  zFilter f (MkUUIDSet us) = MkUUIDSet $ S.filter f us
-  zFilterM f (MkUUIDSet us) =
-    MkUUIDSet . S.fromAscList <$> CM.filterM f (S.toAscList us)
+  zFilter f (MkUUIDSet us) = MkUUIDSet $ filter f us
+  zFilterM f (MkUUIDSet us) = CM.filterM f us >>= return . MkUUIDSet
 
 instance ZEmptiable UUIDSet where
-  zEmpty = MkUUIDSet S.empty
-  zIsNull (MkUUIDSet us) = S.null us
-  zSize (MkUUIDSet us) = S.size us
+  zEmpty = MkUUIDSet []
+  zIsNull (MkUUIDSet us) = null us
+  zSize (MkUUIDSet us) = length us
 
 instance ZIsList UUIDSet UUID where
-  zToList (MkUUIDSet us) = S.toAscList us
-  zFromList = MkUUIDSet . S.fromList
+  zToList (MkUUIDSet us) = us
+  zFromList = MkUUIDSet
