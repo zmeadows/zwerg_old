c5e24a3abfea93c6d9856e9fab2bbf7888e59b10
Zachary Meadows <zmeadows@gmail.com>
2017-07-23 16:19:31 +0200

    give goblin a sword

--- lib/Zwerg/Generator/Enemy/Goblin.hs
+++ lib/Zwerg/Generator/Enemy/Goblin.hs
@@ -2,6 +2,7 @@ module Zwerg.Generator.Enemy.Goblin where
 
 import Data.Text (append)
 import Zwerg.Generator
+import Zwerg.Generator.Item.Weapon
 
 goblin :: Generator UUID
 goblin =
@@ -18,17 +19,18 @@ goblin =
     addComp goblinUUID stats zeroStats
     addComp goblinUUID aiType SimpleMeleeCreature
     addComp goblinUUID blocksPassage True
+    addComp goblinUUID blocksVision False
     assignUniformRandomStat goblinUUID STR (1, 4)
     assignUniformRandomStat goblinUUID DEX (2, 6)
     assignUniformRandomStat goblinUUID INT (1, 2)
     assignUniformRandomStat goblinUUID CHA (1, 2)
     assignUniformRandomStat goblinUUID CON (1, 3)
     assignUniformRandomStat goblinUUID WIS (1, 2)
+    swordUUID <- generate sword
+    equipItem swordUUID goblinUUID
     return goblinUUID
 
-generateGoblinName
-  :: (MonadRandom m)
-  => m Text
+generateGoblinName :: (MonadRandom m) => m Text
 generateGoblinName =
   let firstNameFirstSyllables = ["Gol", "Kra", "Bah", "Quo"]
       firstNameSecondSyllables = ["ith", "xul", "nix", "oth"]
