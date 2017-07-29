26b4b373249749ca2c32863e8480d8aed4ab2b8c
zmeadows <zmeadows@gmail.com>
2017-07-29 14:05:52 +0200

    added more events to the newEvent macro

--- lib/Zwerg/Event.hs
+++ lib/Zwerg/Event.hs
@@ -101,6 +101,9 @@ instance Binary ZwergEventQueue
 class HasZwergEventQueue s where
   eventQueue :: Lens' s ZwergEventQueue
 
+instance HasZwergEventQueue ZwergEventQueue where
+  eventQueue = identity
+
 instance ZWrapped ZwergEventQueue (Seq ZwergEvent) where
   unwrap (MkZwergEventQueue eq) = eq
 
@@ -129,6 +132,8 @@ newEvent :: Language.Haskell.TH.Syntax.Quasi m => Text -> m Exp
 newEvent "MoveEntity"     = runQ [| \a b -> pushEventM $ MoveEntityEvent $ MoveEntityEventData a b|]
 newEvent "IncomingDamage" = runQ [| \a b c d -> pushEventM $ IncomingDamageEvent $ IncomingDamageEventData a b c d|]
 newEvent "OutgoingDamage" = runQ [| \a b c -> pushEventM $ OutgoingDamageEvent $ OutgoingDamageEventData a b c|]
+newEvent "MoveEntityDirection" = runQ [| \a b -> pushEventM $ MoveEntityDirectionEvent $ MoveEntityDirectionEventData a b|]
+newEvent "WeaponAttackAttempt" = runQ [| \a b -> pushEventM $ WeaponAttackAttemptEvent $ WeaponAttackAttemptEventData a b|]
 newEvent _                = runQ [|"INVALID EVENT TYPE PASSED TO TEMPLATE HASKELL FUNCTION 'newEvent'"|]
 
 mergeEventsM :: (HasZwergEventQueue s, MonadState s m)
