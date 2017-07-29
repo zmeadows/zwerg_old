7f4dc735dc2387e0deed0df82ae632c61ed10d0f
zmeadows <zmeadows@gmail.com>
2017-07-29 14:15:21 +0200

    make levelSkeletonGenerator a simple Generator

--- lib/Zwerg/Generator/Level.hs
+++ lib/Zwerg/Generator/Level.hs
@@ -5,24 +5,25 @@ module Zwerg.Generator.Level
 
 import Zwerg.Generator as EXPORTED
 
-levelSkeletonGenerator :: UUID -> Generator' ()
-levelSkeletonGenerator levelUUID =
-  MkGenerator $
+levelSkeletonGenerator :: Generator
+levelSkeletonGenerator = do
+  levelUUID <- popUUID
   let xs = [0 .. mapWidthINT - 1]
       ys = [0 .. mapHeightINT - 1]
-  in do tileList <-
-          forM [(x, y) | x <- xs, y <- ys] $ \(x', y') -> do
-            uuid <- popUUID
-            addComp uuid entityType Tile
-            addComp uuid tileType Void
-            pos <- zConstruct (x', y')
-            addComp uuid position pos
-            addComp uuid occupants zEmpty
-            addComp uuid blocksPassage True
-            addComp uuid needsRedraw True
-            addComp uuid level levelUUID
-            return (pos, uuid)
-        zConstruct tileList >>= setComp levelUUID tileMap
-        setComp levelUUID tiles $ zFromList $ fmap snd tileList
-        setComp levelUUID entityType Level
-        setComp levelUUID name "Test Square Level"
+  tileList <- forM [(x, y) | x <- xs, y <- ys] $
+    \(x', y') -> do
+       uuid <- popUUID
+       addComp uuid entityType Tile
+       addComp uuid tileType Void
+       pos <- zConstruct (x', y')
+       addComp uuid position pos
+       addComp uuid occupants zEmpty
+       addComp uuid blocksPassage True
+       addComp uuid needsRedraw True
+       addComp uuid level levelUUID
+       return (pos, uuid)
+  zConstruct tileList >>= setComp levelUUID tileMap
+  setComp levelUUID tiles $ zFromList $ fmap snd tileList
+  setComp levelUUID entityType Level
+  setComp levelUUID name "Test Square Level"
+  return levelUUID
